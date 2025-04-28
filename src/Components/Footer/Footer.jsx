2fcfc0014c3e933fb5005d4294c4d08c56186a1e
VasileEnzo <enzo.vasile@est.fi.uncoma.edu.ar>
2025-04-28 00:15:38 -0300

    Footer
    Ta vacio jeje

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+
+
+const Footer = () => {  
+       
+        
+        
+    return (
+        <footer className="">
+            
+            
+        </footer>
+     );
+
+
+};
+export default Footer;
